49439fe96fcab5b7a32abd6345b2bac1cb614cca
蒋家焘 <15304072722@163.com>
2019-11-18 10:19:25 +0800

    Initial Commit

---
+++ ModuleJJT1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleJJT1
+//
+//  Created by softium on 2019/11/18.
+//  Copyright © 2019 softium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleJJT1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleJJT1
+//
+//  Created by softium on 2019/11/18.
+//  Copyright © 2019 softium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
